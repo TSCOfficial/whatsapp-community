7594ed19bdb4e631c009ad92e75343b91ac7f5a7
Aaron Frily <thestreakcraft.official@gmail.com>
2025-07-14 19:07:43 +0200

    Fixing file seperation

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@lib': path.resolve(__dirname, './lib')
+    }
+  }
 })
