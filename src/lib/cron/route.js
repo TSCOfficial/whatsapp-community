f159b171343c41215253f0a8b80bda534d439456
Aaron Frily <thestreakcraft.official@gmail.com>
2025-07-14 17:53:18 +0200

    Updated cron

--- src/lib/cron/route.js
+++ src/lib/cron/route.js
@@ -1,10 +1,16 @@
-import { NextResponse } from 'next/server';
 import { Log } from '../logging';
+import Supabase from '../supabase';
 
 export async function GET() {
   if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-    return res.status(401).end('Unauthorized');
+    return new Response("Unauthorized", { status: 401});
   }
+
+  const {data, error} = await Supabase.from("keep-alive")
+  .insert([{name: new Date().toDateString()}])
+  new Log(data)
+  new Log(error, false, true)
+
   new Log("Supabase keepalive executed")
-  return NextResponse.json({ ok: true });
+  return new Response("OK", { status: 200})
 }
--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,8 @@
-
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = `https://${process.env.SUPABASE_ID}.supabase.co`
 const supabaseKey = process.env.SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default function Supabase(){
+    return supabase;
+}
